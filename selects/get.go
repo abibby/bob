bb41d4cdc3eea55c3b6f6b8b498109f57656470d
Adam Bibby <abibby@mero.co>
2023-03-27 09:53:47 -0400

    add load

--- selects/get.go
+++ selects/get.go
@@ -15,61 +15,22 @@ func (b *Builder[T]) Get(tx *sqlx.Tx) ([]T, error) {
 	return b.GetContext(context.Background(), tx)
 }
 func (b *Builder[T]) GetContext(ctx context.Context, tx *sqlx.Tx) ([]T, error) {
-	q, bindings, err := b.ToSQL(dialects.DefaultDialect)
-	if err != nil {
-		return nil, err
-	}
-
 	v := []T{}
-	err = tx.SelectContext(ctx, &v, q, bindings...)
-	if err != nil {
-		return nil, err
-	}
-
-	err = InitializeRelationships(&v)
-	if err != nil {
-		return nil, err
-	}
-
-	err = hooks.AfterLoad(ctx, tx, &v)
-	if err != nil {
-		return nil, err
-	}
-	return v, nil
+	err := b.LoadContext(ctx, tx, &v)
+	return v, err
 }
 
 func (b *Builder[T]) First(tx *sqlx.Tx) (T, error) {
 	return b.FirstContext(context.Background(), tx)
 }
 func (b *Builder[T]) FirstContext(ctx context.Context, tx *sqlx.Tx) (T, error) {
-	lastLimit := b.limit
-	q, bindings, err := b.Limit(1).ToSQL(dialects.DefaultDialect)
-	b.limit = lastLimit
-
-	if err != nil {
-		var zero T
-		return zero, err
-	}
-
 	var v T
 	t := reflect.TypeOf(v)
 	if t.Kind() == reflect.Ptr {
 		v = reflect.New(t.Elem()).Interface().(T)
 	}
-	err = tx.GetContext(ctx, v, q, bindings...)
-	if err != nil {
-		return v, err
-	}
-	err = InitializeRelationships(v)
-	if err != nil {
-		return v, err
-	}
-
-	err = hooks.AfterLoad(ctx, tx, v)
-	if err != nil {
-		return v, err
-	}
-	return v, nil
+	err := b.LoadOneContext(ctx, tx, v)
+	return v, err
 }
 
 func (b *Builder[T]) Find(tx *sqlx.Tx, primaryKeyValues ...any) (T, error) {
@@ -86,4 +47,56 @@ func (b *Builder[T]) FindContext(ctx context.Context, tx *sqlx.Tx, primaryKeyVal
 		b.Where(pKey, "=", primaryKeyValues[i])
 	}
 	return b.FirstContext(ctx, tx)
+}
+
+func (b *Builder[T]) Load(tx *sqlx.Tx, v any) error {
+	return b.LoadContext(context.Background(), tx, v)
+}
+func (b *Builder[T]) LoadContext(ctx context.Context, tx *sqlx.Tx, v any) error {
+	q, bindings, err := b.ToSQL(dialects.DefaultDialect)
+	if err != nil {
+		return err
+	}
+
+	err = tx.SelectContext(ctx, v, q, bindings...)
+	if err != nil {
+		return err
+	}
+
+	err = InitializeRelationships(v)
+	if err != nil {
+		return err
+	}
+
+	err = hooks.AfterLoad(ctx, tx, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *Builder[T]) LoadOne(tx *sqlx.Tx, v any) error {
+	return b.LoadOneContext(context.Background(), tx, v)
+}
+func (b *Builder[T]) LoadOneContext(ctx context.Context, tx *sqlx.Tx, v any) error {
+	q, bindings, err := b.Clone().Limit(1).ToSQL(dialects.DefaultDialect)
+
+	if err != nil {
+		return err
+	}
+
+	err = tx.GetContext(ctx, v, q, bindings...)
+	if err != nil {
+		return err
+	}
+	err = InitializeRelationships(v)
+	if err != nil {
+		return err
+	}
+
+	err = hooks.AfterLoad(ctx, tx, v)
+	if err != nil {
+		return err
+	}
+	return nil
 }
