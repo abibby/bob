b3934cd3ed1656e5c1b61a2e7dff805542b3a090
Adam Bibby <abibby@mero.co>
2023-04-18 14:54:19 -0400

    add semicolons to tests

--- craetetable/builder_test.go
+++ craetetable/builder_test.go
@@ -12,7 +12,7 @@ func TestBuilder(t *testing.T) {
 		{
 			Name:             "create table",
 			Builder:          craetetable.CreateTable("foo", func(t *craetetable.Table) {}),
-			ExpectedSQL:      "CREATE TABLE \"foo\" ()",
+			ExpectedSQL:      "CREATE TABLE \"foo\" ();",
 			ExpectedBindings: []any{},
 		},
 		{
@@ -20,7 +20,7 @@ func TestBuilder(t *testing.T) {
 			Builder: craetetable.CreateTable("foo", func(t *craetetable.Table) {
 				t.String("bar")
 			}),
-			ExpectedSQL:      "CREATE TABLE \"foo\" (\"bar\" TEXT NOT NULL)",
+			ExpectedSQL:      "CREATE TABLE \"foo\" (\"bar\" TEXT NOT NULL);",
 			ExpectedBindings: []any{},
 		},
 		{
@@ -29,7 +29,7 @@ func TestBuilder(t *testing.T) {
 				t.Int("id")
 				t.String("bar")
 			}),
-			ExpectedSQL:      "CREATE TABLE \"foo\" (\"id\" INTEGER NOT NULL, \"bar\" TEXT NOT NULL)",
+			ExpectedSQL:      "CREATE TABLE \"foo\" (\"id\" INTEGER NOT NULL, \"bar\" TEXT NOT NULL);",
 			ExpectedBindings: []any{},
 		},
 	})
